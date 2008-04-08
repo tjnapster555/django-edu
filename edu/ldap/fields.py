a082b989fcd8f6cd5c37e7c843d02ed39fe630d8
myer0052 <myer0052@gmail.com>
2008-04-08 20:42:04 +0000

    Fixed LDAPObjectField to actually work.

--- edu/ldap/fields.py
+++ edu/ldap/fields.py
@@ -1,9 +1,35 @@
 import ldap
 
+from django.utils.translation import ugettext as _
 from django.db import models
 from django.conf import settings
+from django.core import validators
+from django import oldforms
 
-from edu.ldap.utils import LDAPConnection, SecureLDAPConnection
+from edu.ldap.utils import LDAPConnection, SecureLDAPConnection, LDAPItem
+
+class LDAPObject(object):
+    """LDAPObject that is returned when LDAPObjectField attribute is accessed."""
+    
+    def __init__(self, LDAPItem, orig_value):
+        """Create a LDAPObject with the original value saved."""
+        self._ldap_item = LDAPItem
+        self._orig_value = orig_value
+        
+    def __getattr__(self, attribute):
+        return self._ldap_item.__getattr__(attribute)
+
+    def __getitem__(self, attribute):
+        return self._ldap_item.__getitem__(attribute)
+
+    def get(self, attribute, default=None):
+        return self.__getitem__(attribute) or default
+        
+    def __unicode__(self):
+        return u"%s" % self._orig_value
+    
+    def all(self):
+        return unicode(self._ldap_item)
 
 class LdapObjectField(models.Field):
     """This is a special field the stores a DN which relates to a LDAP object.
@@ -14,7 +40,9 @@ class LdapObjectField(models.Field):
        class Person(models.Model):
            name = models.CharField(max_length=255)
            ldap = LdapObjectField(base="dc=directory,dc=State, dc=Edu", 
-                                  server="ldap.state.edu", port=389)
+                                  server="ldap.state.edu", 
+                                  port=389,
+                                  filter_attr='uid')
     
     Then if you added a person to this class you could do the following 
     lookup::
@@ -25,22 +53,34 @@ class LdapObjectField(models.Field):
     """
     __metaclass__ = models.SubfieldBase
     
-    def __init__(self, base=None, server=None, port=None):
+    def __init__(self, verbose_name=None, name=None, filter_attr=None, **kwargs):
         """Setup up ldap object field"""
         # if server/port are null use the defaults from settings.py
-        self.base = base or getattr(settings, 'LDAP_BASE', '')
-        self.server = server or getattr(settings, 'LDAP_SERVER', None)
-        self.port = port or getattr(settings, 'LDAP_SERVER_PORT', 389)
+        self.base = kwargs.get('base') or getattr(settings, 'LDAP_BASE', '')
+        self.server = kwargs.get('server') or getattr(settings, 'LDAP_SERVER', None)
+        self.port = kwargs.get('port') or getattr(settings, 'LDAP_SERVER_PORT', 389)
+        self.filter_attr = filter_attr
+        assert(self.filter_attr, "Must provide a filter_attr")
         self.is_secure = getattr(settings, 'LDAP_SECURE_CONNECTION', False)
         self.username = getattr(settings, 'LDAP_SERVER_USER', '')
         self.password = getattr(settings, 'LDAP_SERVER_USER_PASSWORD', '')
-        return super(LdapObjectField, self).__init__()
+        kwargs['max_length'] = kwargs.get('max_length', 255)
+        models.Field.__init__(self, verbose_name, name, **kwargs)
     
-    def db_type(self):
-        return 'varchar(255)'
+    def get_manipulator_field_objs(self):
+        return [oldforms.TextField]
+
+    def get_internal_type(self):
+        return "CharField"
+
+    def formfield(self, **kwargs):
+        defaults = {'max_length': self.max_length}
+        defaults.update(kwargs)
+        return super(LdapObjectField, self).formfield(**defaults)
     
     def to_python(self, value):
         """Lookup ldap object and return it."""
+        #TODO: cache this method
         if self.is_secure:
             connection = SecureLDAPConnection(self.server, port=self.port,
                                               user=self.username,
@@ -49,4 +89,14 @@ class LdapObjectField(models.Field):
             connection = LDAPConnection(self.server, port=self.port,
                                         user=self.username,
                                         password=self.password)
-        return connection.search(self.base, 'sub', value)
+        filter = "%s=%s" % (self.filter_attr, value)
+        ldap_obj = connection.search(self.base, filter)
+        if len(ldap_obj) != 1:
+            raise validators.ValidationError, _("This filter must return a unique LDAP Object.")
+        return LDAPObject(ldap_obj[0], value)
+    
+    def get_db_prep_save(self, value):
+        # Casts dates into string format for entry into database.
+        if value is not None:
+            value = value._orig_value
+        return models.Field.get_db_prep_save(self, value)
