7e83906b7b8c21b8d897ba6eedf8b9b5ecd17bf1
gary.wilson <gary.wilson@6be534cb-4749-0410-90af-1513945040c4>
2008-05-05 19:18:54 +0000

    Added ActiveManager manager and ImportantDate model.

--- trunk/edu/apps/news/models.py
+++ trunk/edu/apps/news/models.py
@@ -1,5 +1,44 @@
+from datetime import datetime
+
 from django.db import models
 from sitebuilder.models import Section
+
+
+class ActiveManager(models.Manager):
+    """
+    A manager that provides convenience methods for grabbing active/inactive
+    objects.  This manager can be used with any Model with a BooleanField named
+    "active".
+
+
+    Can be used on any model with an active attribute.  Creates methods for
+    returning only active objects or only inactive objects. If the model also
+    has pub_date and/or expire_date you can also use the published or current
+    shortcuts.
+    """
+    def active(self):
+        return self.get_query_set().filter(active=True)
+
+    def inactive(self):
+        return self.get_query_set().filter(active=False)
+
+    def published(self):
+        """
+        Returns active objects whose publication date has passed.  This method
+        can only be used with Models that have a "publish_date" field.
+
+        If the Model has an "expire_date" field, then only active objects whose
+        publication date has passed and whose expiration date has not passed
+        will be returned.
+        """
+        now = datetime.now()
+        objects = self.active().filter(publish_date__lte=now)
+        model_field_names = [field.name for field in self.model._meta.fields]
+        if "expire_date" in model_field_names:
+            objects = objects.filter(
+                Q(expire_date__isnull=True) | Q(expire_date__gt=now))
+        return objects
+
 
 class Story(models.Model):
     """
@@ -18,8 +57,7 @@ class Story(models.Model):
     content = models.TextField(help_text="Place html content here.")
     publish_date = models.DateTimeField("Publication date/time",
         help_text="Enter the date and time you want this story to appear.")
-    active = models.BooleanField(default=True,
-        help_text="Uncheck ")
+    active = models.BooleanField(default=True)
     more = models.ForeignKey("ECEFlatPage", blank=True, null=True, raw_id_admin=True,
         help_text="Link to the full news story this will show as a 'more...' link at the bottom of the content.")
     last_update = models.DateTimeField(auto_now=True)
@@ -97,3 +135,30 @@ class Announcement(models.Model):
                                              self.publish_date.month,
                                              self.publish_date.day,
                                              self.slug])
+
+
+class ImportantDate(models.Model):
+    """
+    This model can be used as a way to publish important dates or deadlines.
+    """
+    sections = models.ManyToManyField(Section,
+        help_text="Select the sections you would like this deadline to be"
+                  " displayed.")
+    text = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True,
+        help_text="If the deadline covers a range of dates, then enter the"
+                  " end date here.")
+    active = models.BooleanField(default=True)
+
+    objects = ActiveManager()
+
+    class Meta:
+        get_latest_by = "date"
+
+    class Admin:
+        list_display = ('text', 'date', 'end_date', 'active')
+        list_filte = ('date', 'active')
+
+    def __unicode__(self):
+        return u"%s - %s" % (unicode(self.date), unicode(self.text))
