5c7c4ed30351ba58fa9f0e8c6dd3b09f5d667103
gary.wilson <gary.wilson@6be534cb-4749-0410-90af-1513945040c4>
2008-06-13 19:24:48 +0000

    Factored some common node code to a `LatestPublicationNode` class that both the story templatetag and new announcements templatetag can use.

--- djangoedu/apps/news/templatetags/edunews.py
+++ djangoedu/apps/news/templatetags/edunews.py
@@ -1,28 +1,36 @@
 from django import template
 
-from djangoedu.apps.news.models import Story, ImportantDate
+from djangoedu.apps.news.models import Story, ImportantDate, Announcement
 
 register = template.Library()
 
 
-class StoryNode(template.Node):
-    def __init__(self, slug, number, var_name):
+class LatestPublicationNode(template.Node):
+    """
+    A template node that is meant to handle various publication models.
+    
+    Model must have ``publish_date`` field and a ``published`` method on its
+    ``objects`` manager.
+    """
+    
+    def __init__(self, model, slug, number, var_name):
+        self.model = model
         self.slug = slug
         self.number = number
         self.var_name = var_name
 
     def render(self, context):
-        stories = Story.objects.published().filter(sections__slug=self.slug
-            ).order_by('-publish_date')[:self.number]
-        print 'stories:', stories
+        objects = self.model.objects.published().filter(
+            sections__slug=self.slug).order_by('-publish_date')
+        if self.number:
+            objects = objects[:self.number]
         if self.number == 1:
-            if stories:
-                context[self.var_name] = stories[0]
+            if objects:
+                context[self.var_name] = objects[0]
             else:
                 context[self.var_name] = None
         else:
-            context[self.var_name] = stories
-        print context
+            context[self.var_name] = objects
         return ''
 
 def story(parser, token):
@@ -32,7 +40,8 @@ def story(parser, token):
     Inputs:
     
     * ``slug`` - Slug of the section to grab news stories for.
-    * ``number`` - The number of news stories to grab.
+    * ``number`` - The number of news stories to grab.  If zero, then grab all
+                   stories for the section.
     * ``variable`` - The name of the template variable to save the stories to.
     
     Syntax::
@@ -43,21 +52,53 @@ def story(parser, token):
     
         {% story "grad" 1 as story %}
     """
+    return LatestPublicationNode(Story, *publication_parse_tokens(token))
+register.tag('story', story)
+
+def announcements(parser, token):
+    """
+    Returns the latest announcement or announcements.
+    
+    Inputs:
+    
+    * ``slug`` - Slug of the section to grab announcements for.
+    * ``number`` - The number of announcements to grab.  If zero, then grab all
+                   announcements for the section.
+    * ``variable`` - The name of the template variable to save the
+                     announcements to.
+    
+    Syntax::
+    
+        {% announcements <slug> <number> as <variable> %}
+        
+    Example::
+    
+        {% announcements "grad" 1 as story %}
+    """
+    return LatestPublicationNode(Announcement, *publication_parse_tokens(token))
+register.tag('announcements', announcements)
+
+def publication_parse_tokens(token):
+    """
+    Take a token from a publication templatetag parser function and return a
+    3-tuple of (slug, number, variable).
+    """
     tokens = token.split_contents()
     usage = ("Tag should be in the form of:"
-             " {% story <slug> <number> as <variable> %}")
+             " {%% %s <slug> <number> as <variable> %%}" % tokens[0])
     if len(tokens) != 5 or tokens[3] != 'as':
         raise template.TemplateSyntaxError(usage)
     try:
         number = int(tokens[2])
     except ValueError:
         raise template.TemplateSyntaxError("number must be an integer.  " + usage)
+    if number < 0:
+        raise template.TemplateSyntaxError("number must be a positive integer.  " + usage)
     slug = tokens[1]
     if (slug.startswith('"') and slug.endswith('"')
             or (slug.startswith("'") and slug.endswith("'"))):
         slug = slug[1:-1]
-    return StoryNode(slug, number, tokens[4])
-register.tag('story', story)
+    return (slug, number, tokens[4])
 
 
 class DatesNode(template.Node):
